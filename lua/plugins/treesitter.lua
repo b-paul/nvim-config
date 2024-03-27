d2469642a7102f6503a38a09013ab954f152af2c
Benjamin Paul <bpaul@bpaul.xyz>
2024-03-27 12:06:08 +1000

    Stop treesitter from trying to install already installed parsers, and also enable auto update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,8 @@
 return function()
     require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-            "rust",
-            "lua",
-            "haskell",
-            "c",
-            "vimdoc",
-            "regex", -- ?
-        },
+        auto_install = true,
         sync_install = false,
         highlight = {
-            enable = true,
-        },
-        indent = {
             enable = true,
         },
     })
