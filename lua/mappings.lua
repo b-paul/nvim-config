ad7c398e36db644763c5bc845d115009c8866d6c
Benjamin Paul <bpaul@bpaul.xyz>
2025-08-19 23:09:25 +1000

    Remove old lsp keybindings (I shall use the new default ones)

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -16,17 +16,7 @@ end
 M.lsp = function(_, bufnr)
     local lsp_opts = { silent = true, buffer = bufnr }
     vim.keymap.set("n", "<leader><leader>f", vim.lsp.buf.format, lsp_opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, lsp_opts)
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, lsp_opts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, lsp_opts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, lsp_opts)
-    vim.keymap.set("n", "<leader>d", vim.lsp.buf.type_definition, lsp_opts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, lsp_opts)
-    vim.keymap.set("n", "<leader><leader>r", vim.lsp.buf.rename, lsp_opts)
-    vim.keymap.set("n", "<leader><leader>c", vim.lsp.buf.code_action, lsp_opts)
     vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 end
 
 M.telescope = function()
